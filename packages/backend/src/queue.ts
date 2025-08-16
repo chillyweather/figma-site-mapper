8f3592b5fc410826a269b7b784453f2c6333d4e5
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-08-16 15:18:37 +0300

    started working on manifest

--- packages/backend/src/queue.ts
+++ packages/backend/src/queue.ts
@@ -1,6 +1,6 @@
 import { Queue } from 'bullmq';
 import { Redis } from 'ioredis';
 
-export const connection = new Redis();
+export const connection = new Redis({ maxRetriesPerRequest: null });
 
 export const crawlQueue = new Queue('crawl-jobs', { connection });
